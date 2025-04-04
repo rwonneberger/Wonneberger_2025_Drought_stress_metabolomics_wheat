2268095de62da90bcb3f7766170a6706fb993320
Ronja Wonneberger <70649078+rwonneberger@users.noreply.github.com>
2025-04-04 16:33:16 +0200

    Update 0_Preprocessing_Filtering_Harvest_data.R

--- 0_Preprocessing_Filtering_Harvest_data.R
+++ 0_Preprocessing_Filtering_Harvest_data.R
@@ -1,16 +1,13 @@
-# Analysis of Marvin, tiller data in exp2
+# filtering of raw post-harvest data
 
 source("https://raw.githubusercontent.com/rwonneberger/R_functions/main/Ronja_functions.R")
 library(lme4)
 library(multcompView)
 library(psych)
 
-exp = "Exp2"
-
 setwd("//storage-al.slu.se/home$/rawo0001/My Documents/Projects/CresWheat/Manuscripts/Metabolome_Drought_stress/Scripttest/")
 
-df<-fread("Data/Harvest_data2.txt")
-
+df<-fread("Data/Harvest_data2.txt") # Additional file 4
 
 df_melt<-melt(df, id.vars = c("PlantID", "Genotype", "Treatment"))
 df_melt$value<-as.numeric(df_melt$value)
@@ -21,11 +18,8 @@ df_filt<-df_melt%>%
   filter(!any(num_na >=18)) %>%
   select(-num_na)%>%ungroup
 
-
-
-
 df_in<-as.data.table(dcast(df_filt,  PlantID + Genotype + Treatment ~ variable, value.var = "value"))
 
 df_in$PeduncleLength<-df_in$Length_SpikeBase_FlagLeaf<-df_in$SpikePlantRatioWeight<-df_in$GrainPlantRatioWeight<-df_in$LengthLastInternode<-NULL
 
-fwrite(df_in, "Data/Harvest_data_filtered2.txt")
+fwrite(df_in, "Data/Harvest_data_filtered2.txt") 
